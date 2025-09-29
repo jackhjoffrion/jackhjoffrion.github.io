6728979ad855b5b41897ad7c4c9019260ceb7ce6
jackhjoffrion <jackhjoffrion@gmail.com>
2025-09-29 16:36:05 +0000

    Editted Platformer

--- fsd-projects/platformer/helpers.js
+++ fsd-projects/platformer/helpers.js
@@ -373,14 +373,14 @@ function deathOfPlayer() {
   ctx.fillStyle = "black";
   ctx.font = "800% serif";
   ctx.fillText(
-    "You are dead",
+    "You Have Died",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 5,
     (canvas.width / 16) * 14
   );
   ctx.font = "500% serif";
   ctx.fillText(
-    "Hit any key to restart",
+    "Press any key to restart",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 3,
     (canvas.width / 16) * 14
